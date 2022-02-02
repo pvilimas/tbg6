182e482d3f756fef995ff24d44fe832893728f74
Paul Vilimas <paul.vilimas@gmail.com>
2022-02-02 14:53:27 -0500

    prototype

--- main.cpp
+++ main.cpp
@@ -4,16 +4,46 @@
 
 using namespace std;
 
+// displayed at the start of lines
+const string prompt = "> ";
+
+class TextArea {
+    int x, y, width, height;
+    string text;
+    
+    public:
+
+        TextArea(int xpos, int ypos, int w=100, int h=100) {
+            x = xpos;
+            y = ypos;
+            width = w;
+            height = h;
+            text = "text will go here\n> more stuff here\n> go north";
+        }
+
+        void draw(void) {
+            DrawRectangle(x, y, width, height, BLACK);
+            DrawText((prompt + text).c_str(), x, y, 20, WHITE);
+        }
+};
+
 int main() {
-    InitWindow(800, 500, "textbasedgame");
+    InitWindow(640, 460, "textbasedgame");
     SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_VSYNC_HINT | FLAG_WINDOW_RESIZABLE);
     SetTargetFPS(60);
+
+    auto img = LoadImage("assets/a.png");
+    auto texture = LoadTextureFromImage(img);
+    UnloadImage(img);
+
+    TextArea t = TextArea { 0, 360, 640, 100 };
 
     while (!WindowShouldClose()) {
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
-        DrawText("a", 100, 100, 20, BLACK);
+        DrawTexture(texture, 0, 0, WHITE);
+        t.draw();
         
         EndDrawing();
     }
